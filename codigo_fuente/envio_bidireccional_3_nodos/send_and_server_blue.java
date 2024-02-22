46bd65b46577f55028f4a0e48953fb08a4ab2f84
JorgeBernalRomero <JorgeBernalRomero@users.noreply.github.com>
2024-02-22 12:15:56 +0100

    Subiendo codigo con 3 nodos
    El código funciona pero en el nodo blue en los archivos de results solo guarda el resultado del "ls" (script_green.sh). Sin embargo, en las máquinas green y pink ejecuta correctamente ls y ls -l respectivamente.

---
+++ codigo_fuente/envio_bidireccional_3_nodos/send_and_server_blue.java
@@ -0,0 +1,246 @@
+package testDelivery;
+
+import java.io.*;
+import javax.jms.*;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+
+public class send_and_server {
+    //Class for files management
+	public static class FileManager {
+		public static byte[] readFileAsBytes(File file) throws IOException {
+			try (RandomAccessFile accessFile = new RandomAccessFile(file, "r")){
+				byte[] bytes = new byte[(int) accessFile.length()];
+				accessFile.readFully(bytes);
+				return bytes;
+			}
+		}
+
+		public void writeFileAsBytes(byte[] bytes, String fileName) throws IOException {
+			File file = new File(fileName);
+			try (RandomAccessFile accessFile = new RandomAccessFile(file, "w")){
+				accessFile.write(bytes);
+			}
+		}
+
+		public static void writeBytesToFile(File receivedFile, byte[] fileBytes) throws IOException {
+			try(FileOutputStream fos = new FileOutputStream(receivedFile)){
+				fos.write(fileBytes);
+			}
+		}
+	}
+
+
+
+    //Function which makes node to listen and await for the income message
+	public static void listeningFromGreen (Session session){
+		try{
+			MessageConsumer consumer = session.createConsumer(session.createQueue("domibus.backend.jms.outQueue"));
+			Message msg = consumer.receive(); //podríamos poner 10000 que serían 10 segundos
+		    System.out.println(msg);
+
+			MapMessage m = (MapMessage) msg;
+			if (msg instanceof MapMessage) {
+				System.out.println(msg.getJMSMessageID());
+				System.out.println(m.getBytes("payload_1"));
+				System.out.println(new String(m.getBytes("payload_1"),"UTF-8"));
+
+				String saveDirectory = "/root/TFG/recepcionResults/greenResults"; //esta parte se podría sustituir pasando el dirDestino por parámetro
+				File saveDir = new File(saveDirectory);
+				if (!saveDir.exists()) {
+        			saveDir.mkdirs();
+        		}
+
+				File destinationFile = new File(saveDir, "payload_1");
+				FileManager.writeBytesToFile(destinationFile, m.getBytes("payload_1"));
+        		System.out.println("File saved to: " + destinationFile.getAbsolutePath());
+
+
+        		System.out.println("Got a message: ");
+			    System.out.println(msg);
+			 }
+
+			else{
+			    String payload = "No Message Found!";
+			    System.out.println(payload);
+			}
+
+			System.out.println("fin");
+
+			consumer.close();
+
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+
+	    //Function which makes node to listen and await for the income message
+		public static void listeningFromPink (Session session){
+			try{
+				MessageConsumer consumer = session.createConsumer(session.createQueue("domibus.backend.jms.outQueue"));
+				Message msg = consumer.receive(); //podríamos poner 10000 que serían 10 segundos
+				System.out.println(msg);
+
+				MapMessage m = (MapMessage) msg;
+				if (msg instanceof MapMessage) {
+					System.out.println(msg.getJMSMessageID());
+					System.out.println(m.getBytes("payload_1"));
+					System.out.println(new String(m.getBytes("payload_1"),"UTF-8"));
+
+					String saveDirectory = "/root/TFG/recepcionResults/pinkResults"; //esta parte se podría sustituir pasando el dirDestino por parámetro
+					File saveDir = new File(saveDirectory);
+					if (!saveDir.exists()) {
+						saveDir.mkdirs();
+					}
+
+					File destinationFile = new File(saveDir, "payload_1");
+					FileManager.writeBytesToFile(destinationFile, m.getBytes("payload_1"));
+					System.out.println("File saved to: " + destinationFile.getAbsolutePath());
+
+
+					System.out.println("Got a message: ");
+					System.out.println(msg);
+				 }
+
+				else{
+					String payload = "No Message Found!";
+					System.out.println(payload);
+				}
+
+				System.out.println("fin");
+
+				consumer.close();
+
+			}
+			catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+
+
+
+    public static void sendingToGreen(Session session){
+		try{
+
+			MessageProducer producer = null;
+			Destination destination = session.createQueue("domibus.backend.jms.inQueue");
+			producer = session.createProducer(destination);
+
+			producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+			MapMessage messageMap = session.createMapMessage();
+
+			messageMap.setStringProperty("messageType", "submitMessage");
+			messageMap.setStringProperty("service","bdx:noprocess");
+			messageMap.setStringProperty("serviceType","tc1");
+			messageMap.setStringProperty("action", "TC1Leg1");
+			messageMap.setStringProperty("fromPartyId", "domibus-blue"); //nodo inicial
+			messageMap.setStringProperty("fromPartyType", "urn:oasis:names:tc:ebcore:partyid-type:unregistered");
+			messageMap.setStringProperty("toPartyId", "domibus-green"); //nodo destino
+			messageMap.setStringProperty("toPartyType", "urn:oasis:names:tc:ebcore:partyid-type:unregistered");
+			messageMap.setStringProperty("fromRole", "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/initiator");
+			messageMap.setStringProperty("toRole", "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/responder");
+			messageMap.setStringProperty("originalSender", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C1");
+			messageMap.setStringProperty("finalRecipient", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4");
+			messageMap.setStringProperty("protocol", "AS4");
+			messageMap.setJMSCorrelationID("12345");
+			messageMap.setStringProperty("totalNumberOfPayloads", "1");
+
+			//messageMap.setStringProperty("payload_1_mimeContentId", "cid:file-attached");
+			messageMap.setStringProperty("payload_1_mimeContentId", "cid:message");
+			messageMap.setStringProperty("payload_1_mimeType", "text/xml");
+			messageMap.setStringProperty("payload_1_description", "message");
+
+			//Sending a file from one node to another works (testing script)
+            File file = new File("/root/TFG/envioScripts/script_green.sh"); //directorio puede modificarse o por parámetro???
+			messageMap.setBytes("payload_1", FileManager.readFileAsBytes(file));
+
+			producer.send(messageMap);
+			System.out.println(messageMap);
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+    public static void sendingToPink(Session session){
+		try{
+
+			MessageProducer producer = null;
+			Destination destination = session.createQueue("domibus.backend.jms.inQueue");
+			producer = session.createProducer(destination);
+
+			producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+			MapMessage messageMap = session.createMapMessage();
+
+			messageMap.setStringProperty("messageType", "submitMessage");
+			messageMap.setStringProperty("service","bdx:noprocess");
+			messageMap.setStringProperty("serviceType","tc1");
+			messageMap.setStringProperty("action", "TC1Leg1");
+			messageMap.setStringProperty("fromPartyId", "domibus-blue"); //nodo inicial
+			messageMap.setStringProperty("fromPartyType", "urn:oasis:names:tc:ebcore:partyid-type:unregistered");
+			messageMap.setStringProperty("toPartyId", "domibus-pink"); //nodo destino
+			messageMap.setStringProperty("toPartyType", "urn:oasis:names:tc:ebcore:partyid-type:unregistered");
+			messageMap.setStringProperty("fromRole", "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/initiator");
+			messageMap.setStringProperty("toRole", "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/responder");
+			messageMap.setStringProperty("originalSender", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C1");
+			messageMap.setStringProperty("finalRecipient", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4");
+			messageMap.setStringProperty("protocol", "AS4");
+			messageMap.setJMSCorrelationID("12345");
+			messageMap.setStringProperty("totalNumberOfPayloads", "1");
+
+			//messageMap.setStringProperty("payload_1_mimeContentId", "cid:file-attached");
+			messageMap.setStringProperty("payload_1_mimeContentId", "cid:message");
+			messageMap.setStringProperty("payload_1_mimeType", "text/xml");
+			messageMap.setStringProperty("payload_1_description", "message");
+
+			//Sending a file from one node to another works (testing script)
+            File file = new File("/root/TFG/envioScripts/script_pink.sh"); //directorio puede modificarse o por parámetro???
+			messageMap.setBytes("payload_1", FileManager.readFileAsBytes(file));
+
+			producer.send(messageMap);
+			System.out.println(messageMap);
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+
+    //main code
+	public static void main(String[] args){
+		try{
+			//Connecting to the ActiveMQ connection factory
+			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616"); //URL del servidor ActiveMQ
+			Connection connection = null;
+
+			connection = connectionFactory.createConnection("admin", "123456"); //username and password of the default JMS broker
+			connection.start();
+
+
+			Session sessionWGreen = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Session sessionWPink = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+			//Calling function
+			sendingToGreen(sessionWGreen);
+			sendingToPink(sessionWPink);
+
+			//Calling function
+			listeningFromGreen(sessionWGreen);
+			listeningFromPink(sessionWPink);
+
+			connection.close();
+			System.out.println("Connection ends");
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+}

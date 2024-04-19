b9969dcc8b115968a190b78ce2605cee480c61e7
JorgeBernalRomero <JorgeBernalRomero@users.noreply.github.com>
2024-04-19 10:15:40 +0200

    Update Sender.java
    Simplemente pongo el string directamente en la nueva propiedad de jms.

--- codigo_fuente/proof_of_concept/src/main/java/eu/bigan/fed/edelivery/jms/Sender.java
+++ codigo_fuente/proof_of_concept/src/main/java/eu/bigan/fed/edelivery/jms/Sender.java
@@ -29,8 +29,7 @@ public class Sender{
             messageMap.setStringProperty("protocol", "AS4");
             messageMap.setStringProperty("conversationId", messageId);
             
-            String customFieldName = "workerTask_id";
-            messageMap.setStringProperty(customFieldName, workerTask);
+            messageMap.setStringProperty("workerTask_id", workerTask);
             
             messageMap.setJMSCorrelationID("12345");
             messageMap.setStringProperty("totalNumberOfPayloads", "1");

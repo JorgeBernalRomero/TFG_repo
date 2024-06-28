db68ed071065c38149194a079c4b90fe8e73a431
JorgeBernalRomero <JorgeBernalRomero@users.noreply.github.com>
2024-06-28 20:58:31 +0200

    Create StatusManager.java

---
+++ codigo_fuente/proof_of_concept/src/main/java/eu/bigan/fed/edelivery/utils/StatusManager.java
@@ -0,0 +1,17 @@
+package eu.bigan.fed.edelivery.utils;
+
+
+public class StatusManager{
+	private int status;
+	
+	public StatusManager(int status) {
+		this.status=status;
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+	
+	//PENDIENTE IMPLEMENTAR EL RESTO DE COSAS
+	
+}
